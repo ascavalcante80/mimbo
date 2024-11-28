ff84463f3687bd72bd73e711beb4eb282f668d30
alexandre <ascavalcante80@gmail.com>
2024-11-28 20:53:16 +0100

    MIM-10 Creates firebase_manager.dart

---
+++ lib/data/repositories/firebase_manager.dart
@@ -0,0 +1,72 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../models/users.dart';
+
+class FirestoreManager {
+  final FirebaseFirestore firestore;
+  late final String userId;
+
+  late CollectionReference users;
+  late CollectionReference items;
+  late CollectionReference reactions;
+  late CollectionReference usernames;
+  late CollectionReference userCreatedItems;
+  final String reactionPerformedCollectionName = 'reactions_performed';
+
+  FirestoreManager({required this.userId, required this.firestore}) {
+    users = firestore.collection('users');
+    items = firestore.collection('items');
+    reactions = firestore.collection('reactions');
+    usernames = firestore.collection('usernames');
+    userCreatedItems = users.doc(userId).collection('created_items');
+  }
+
+  FirestoreManager.noUserId({required this.firestore}) {
+    users = firestore.collection('users');
+    items = firestore.collection('items');
+    reactions = firestore.collection('reactions');
+    usernames = firestore.collection('usernames');
+  }
+
+  Future<MimUser?> getUserByID(String id) async {
+    /// The [getUserByID] method is a method that searches for a [MimUser] in
+    /// the Firestore database by its ID. It returns the [MimUser] if it
+    /// exists, otherwise it returns null.
+
+    MimUser? mimUser;
+    await users.doc(id).get().then((value) {
+      if (value.exists) {
+        mimUser = MimUser.fromDocumentSnapshot(value);
+      }
+    });
+    return mimUser;
+  }
+
+  //
+  // Future<void> createUser(GnomeeUser user) async {
+  //   /// The [createUser] method is a method that creates a new [GnomeeUser] in the
+  //   /// Firestore database. It uses the UUID of the user as the document ID.
+  //   ///
+  //
+  //   DocumentReference userDoc = users.doc(user.id);
+  //
+  //   await userDoc.get().then((value) {
+  //     if (value.exists) {
+  //       throw IDAlreadyExistsException();
+  //     }
+  //   });
+  //
+  //   await users.where('username', isEqualTo: user.username).get().then((value) {
+  //     if (value.docs.isNotEmpty) {
+  //       throw UsernameAlreadyExistsException();
+  //     }
+  //   });
+  //   await users.doc(user.id).set(user.toJson());
+  // }
+
+
+}
