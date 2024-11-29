b13a211dd0ea25c5600783baa12e4490bb82e35e
alexandre <ascavalcante80@gmail.com>
2024-11-29 15:23:43 +0100

    MIM-10 Creates SignOutAppBarAction

--- lib/presentation/widget/widgets.dart
+++ lib/presentation/widget/widgets.dart
@@ -6,6 +6,10 @@ import 'package:flutter/material.dart';
 import '../screens/login_screen.dart';
 
 class GoToLoginScreenButton extends StatelessWidget {
+  /// The [GoToLoginScreenButton] is a button that allows the user to navigate
+  /// to the login screen. It disconnects the user from the Firebase Auth
+  /// instance before navigating to the login screen.
+
   const GoToLoginScreenButton({super.key});
 
   @override
@@ -24,6 +28,32 @@ class GoToLoginScreenButton extends StatelessWidget {
       },
       // child: Text(AppLocalizations.of(context)!.goToLoginMessage),
       child: const Text('Go to login screen'),
+    );
+  }
+}
+
+class SignOutAppBarAction extends StatelessWidget {
+  /// The [SignOutAppBarAction] is a button that allows the user to navigate
+  /// to the login screen. It disconnects the user from the Firebase Auth
+  /// instance before navigating to the login screen.
+
+  const SignOutAppBarAction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () async {
+        log('Signing user out!');
+        await FirebaseAuth.instance.signOut();
+
+        if (context.mounted) {
+          Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => const AuthGate()),
+            (route) => false,
+          );
+        }
+      },
+      icon: const Icon(Icons.logout),
     );
   }
 }
