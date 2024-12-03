bc0e0e995cf051b4a0342a5bfabe3a88981bb2ff
alexandre <ascavalcante80@gmail.com>
2024-12-03 22:46:22 +0100

    MIM-18 Creates Home Screen

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,12 +1,26 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'login_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
 // TODO place holder for home screen
   static const String routeName = '/home_screen';
+
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentPageIndex = 0;
+  List<Widget> pages = <Widget>[
+    const LabRoom(),
+    const FeedScreen(),
+    const ProfileScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -14,24 +28,84 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Screen'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const Text('Welcome to the Home Screen'),
-            ElevatedButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-                if (!context.mounted) return;
-                Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(builder: (context) => const AuthGate()),
-                  (route) => false,
-                );
-              },
-              child: const Text('Go to Login Screen'),
+      body: pages[currentPageIndex],
+      bottomNavigationBar: NavigationBar(
+        elevation: 10,
+        shadowColor: Colors.blueGrey,
+        height: 60,
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        // indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home, color: Colors.amber),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            selectedIcon: Text(
+              '🔥',
+              style: TextStyle(fontSize: 22),
             ),
-          ],
-        ),
+            icon: Icon(FontAwesomeIcons.fireFlameCurved),
+            label: 'Feed',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.account_box, color: Colors.amber),
+            icon: Icon(Icons.account_box_outlined),
+            label: 'Profile',
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: const [
+          Text('Welcome to the Feed Screen'),
+          Text('🔥'),
+        ],
+      ),
+    );
+  }
+}
+
+class LabRoom extends StatelessWidget {
+  const LabRoom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Text('Welcome to the Lab Room'),
+        Text('🧪'),
+      ],
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Text('Welcome to the Profile Screen'),
+        Text('👤'),
+      ],
     );
   }
 }
