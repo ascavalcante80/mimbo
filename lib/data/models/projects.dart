9e04cbf78265b0af2dbd68785c7188f329756e11
alexandre <ascavalcante80@gmail.com>
2024-12-04 14:57:03 +0100

    MIM-65 Add factory method to Project class

--- lib/data/models/projects.dart
+++ lib/data/models/projects.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 
 class Project extends Equatable {
@@ -16,20 +17,20 @@ class Project extends Equatable {
   final List<String> unreadAnswersIds;
   final List<String> feedbackAssessmentIds;
 
-  Project(
-    this.id,
-    this.name,
-    this.description,
-    this.category,
-    this.createdAt,
-    this.officialUrl,
-    this.installationUrls,
-    this.keywords,
-    this.languages,
-    this.screenshotsPics,
-    this.unreadAnswersIds,
-    this.feedbackAssessmentIds,
-  ) {
+  Project({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.category,
+    required this.createdAt,
+    required this.officialUrl,
+    required this.installationUrls,
+    required this.keywords,
+    required this.languages,
+    required this.screenshotsPics,
+    required this.unreadAnswersIds,
+    required this.feedbackAssessmentIds,
+  }) {
     assert(id.isNotEmpty);
     assert(name.isNotEmpty);
     assert(description.isNotEmpty);
@@ -37,6 +38,58 @@ class Project extends Equatable {
     assert(officialUrl.isNotEmpty);
     assert(keywords.isNotEmpty);
     assert(languages.isNotEmpty);
+  }
+
+  // function to create JSON from project
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'category': category,
+      'created_at': createdAt,
+      'official_url': officialUrl,
+      'installation_urls': installationUrls,
+      'keywords': keywords,
+      'languages': languages,
+      'screenshots_pics': screenshotsPics,
+      'unread_answers_ids': unreadAnswersIds,
+      'feedback_assessment_ids': feedbackAssessmentIds,
+    };
+  }
+
+  factory Project.fromJson(Map<String, dynamic> json) {
+    return Project(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      category: json['category'],
+      createdAt: json['created_at'].toDate(),
+      officialUrl: json['official_url'],
+      installationUrls: json['installation_urls'],
+      keywords: json['keywords'],
+      languages: json['languages'],
+      screenshotsPics: json['screenshots_pics'],
+      unreadAnswersIds: json['unread_answers_ids'],
+      feedbackAssessmentIds: json['feedback_assessment_ids'],
+    );
+  }
+
+  factory Project.fromDocumentSnapshot(DocumentSnapshot documentSnapshot) {
+    return Project(
+      id: documentSnapshot.id,
+      name: documentSnapshot['name'],
+      description: documentSnapshot['description'],
+      category: documentSnapshot['category'],
+      createdAt: documentSnapshot['created_at'].toDate(),
+      officialUrl: documentSnapshot['official_url'],
+      installationUrls: documentSnapshot['installation_urls'],
+      keywords: documentSnapshot['keywords'],
+      languages: documentSnapshot['languages'],
+      screenshotsPics: documentSnapshot['screenshots_pics'],
+      unreadAnswersIds: documentSnapshot['unread_answers_ids'],
+      feedbackAssessmentIds: documentSnapshot['feedback_assessment_ids'],
+    );
   }
 
   @override
