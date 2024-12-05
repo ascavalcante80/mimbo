90df0ba8ee970a9974e94ac5fa4a26ae0b823728
alexandre <ascavalcante80@gmail.com>
2024-12-05 17:23:00 +0100

    MIM-16 Adds factory methods

--- lib/data/models/feed_info.dart
+++ lib/data/models/feed_info.dart
@@ -1,4 +1,8 @@
-class FeedInfo {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:equatable/equatable.dart';
+
+// ignore: must_be_immutable
+class FeedInfo extends Equatable {
   final String id;
   final String ownerId;
   DateTime dateCursor;
@@ -11,5 +15,36 @@ class FeedInfo {
     required this.dateCursor,
     required this.testsIdsSeen,
     required this.testInProgressId,
-  });
+  }) {
+    assert(id.trim().isNotEmpty);
+    assert(ownerId.trim().isNotEmpty);
+  }
+
+  factory FeedInfo.fromDocumentSnapshot(DocumentSnapshot documentSnapshot) {
+    final data = documentSnapshot.data() as Map<String, dynamic>;
+    return FeedInfo.fromJson(data);
+  }
+
+  factory FeedInfo.fromJson(Map<String, dynamic> json) {
+    return FeedInfo(
+      id: json['id'],
+      ownerId: json['owner_id'],
+      dateCursor: json['date_cursor'].toDate(),
+      testsIdsSeen: List<String>.from(json['tests_ids_seen']),
+      testInProgressId: json['test_in_progress_id'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'owner_id': ownerId,
+      'date_cursor': dateCursor,
+      'tests_ids_seen': testsIdsSeen,
+      'test_in_progress_id': testInProgressId,
+    };
+  }
+
+  @override
+  // Note: The `dateCursor` field is not included in the `props` list.
+  List<Object?> get props => [id, ownerId, testsIdsSeen, testInProgressId];
 }
