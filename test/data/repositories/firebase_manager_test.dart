ebf03a83c443f5bc9694da5a555623587125ee40
alexandre <ascavalcante80@gmail.com>
2024-12-05 17:29:25 +0100

    MIM-16 Removes unused imports

--- test/data/repositories/firebase_manager_test.dart
+++ test/data/repositories/firebase_manager_test.dart
@@ -1,23 +1,12 @@
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mimbo/bloc/cubits/project_cubit.dart';
+
 import 'package:mimbo/data/constants.dart';
 import 'package:mimbo/data/models/credits.dart';
 import 'package:mimbo/data/models/feedback_form.dart';
 import 'package:mimbo/data/models/projects.dart';
-
-// import 'package:gnomee/constants/enums.dart';
-// import 'package:gnomee/data/models/user.dart';
-// import 'package:gnomee/data/repositories/firebase_manager.dart';
-// import 'package:gnomee/data/repositories/user_manager.dart';
-// import 'package:gnomee/utils/date_tools.dart';
 import 'package:mimbo/data/models/users.dart';
 import 'package:mimbo/data/repositories/firebase_manager.dart';
-import 'package:mimbo/data/repositories/project_manager.dart';
-import 'package:mimbo/data/repositories/user_manager.dart';
 
 void main() {
   group('project manager Tests', () {
@@ -160,7 +149,6 @@ void main() {
     });
 
     test('Test CRUD operations for MimCredit', () async {
-
       DateTime before = DateTime(2021, 1, 1);
       DateTime after = DateTime(2022, 2, 1);
       MimCredit credit = MimCredit(
