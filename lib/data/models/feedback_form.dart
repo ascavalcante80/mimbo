878fcf23c97c42a030cb0146f28bbf0cddd8e344
alexandre <ascavalcante80@gmail.com>
2024-12-05 16:09:08 +0100

    MIM-66 Renames class to FormFeedback refactors createdAt field

--- lib/data/models/feedback_form.dart
+++ lib/data/models/feedback_form.dart
@@ -1,37 +1,41 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:equatable/equatable.dart';
 
 import '../constants.dart';
 
-class FeedbackForm {
+class FormFeedback extends Equatable {
   final String id;
   final String userId;
   final String projectId;
   final String openTestId;
+  final DateTime createdAt;
   String? openFeedback;
   final FeedbackOptions feedbackOption;
 
-  FeedbackForm({
+  FormFeedback({
     required this.id,
     required this.userId,
     required this.projectId,
     required this.openTestId,
     required this.openFeedback,
     required this.feedbackOption,
+    required this.createdAt,
   });
 
-  factory FeedbackForm.fromJson(Map<String, dynamic> json) {
-    return FeedbackForm(
+  factory FormFeedback.fromJson(Map<String, dynamic> json) {
+    return FormFeedback(
       id: json['id'],
       userId: json['userId'],
       projectId: json['projectId'],
       openTestId: json['openTestId'],
       openFeedback: json['openFeedback'],
       feedbackOption: FeedbackOptions.values[json['feedbackOption']],
+      createdAt: DateTime.parse(json['createdAt']),
     );
   }
 
-  factory FeedbackForm.fromDocumentSnapshot(DocumentSnapshot documentSnapshot) {
-    return FeedbackForm(
+  factory FormFeedback.fromDocumentSnapshot(DocumentSnapshot documentSnapshot) {
+    return FormFeedback(
       id: documentSnapshot.id,
       userId: documentSnapshot['userId'],
       projectId: documentSnapshot['projectId'],
@@ -39,17 +43,23 @@ class FeedbackForm {
       openFeedback: documentSnapshot['openFeedback'],
       feedbackOption:
           FeedbackOptions.values[documentSnapshot['feedbackOption']],
+      createdAt: documentSnapshot['createdAt'].toDate(),
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
       'userId': userId,
       'projectId': projectId,
       'openTestId': openTestId,
       'openFeedback': openFeedback,
       'feedbackOption': feedbackOption.name,
+      'createdAt': FieldValue.serverTimestamp(),
     };
   }
+
+  @override
+  // Important to notice, the createdAt field is not included in the props list.
+  List<Object?> get props =>
+      [id, userId, projectId, openTestId, openFeedback, feedbackOption];
 }
