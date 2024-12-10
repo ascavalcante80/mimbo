f585ffd3140a9b4a8672f3d0f7c7d2b2c5e97d66
alexandre <ascavalcante80@gmail.com>
2024-12-10 22:14:59 +0100

    MIM-73 Removes old user state

--- lib/logic/cubits/user_cubit.dart
+++ lib/logic/cubits/user_cubit.dart
@@ -2,24 +2,21 @@ import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:mimbo/logic/bloc/user_bloc.dart';
 
 import '../../data/constants.dart';
 import '../../data/models/users.dart';
 
-part 'user_state.dart';
+class MimUserCubit extends Cubit<UserState> {
+  MimUserCubit() : super(UserInitialState());
 
-class MimUserCubit extends Cubit<MimUserState> {
-  MimUserCubit() : super(MimUserState(user: null));
-
-  void updateUser(MimUser user) => emit(MimUserState(user: user));
+  void updateUser(MimUser user) => emit(UserUpdatedState(user: user));
 
   @override
-  void onChange(Change<MimUserState> change) {
+  void onChange(Change<UserState> change) {
     // TODO: implement onChange
     log('current user state :${change.currentState.user} => next user state :${change.nextState.user}');
 
     super.onChange(change);
   }
-
-
 }
