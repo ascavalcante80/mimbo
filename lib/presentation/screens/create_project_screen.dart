8878c30a5f86377e5e01ef979ef80d27438ca85d
alexandre <ascavalcante80@gmail.com>
2024-12-10 22:13:48 +0100

    MIM-73 Removes bloc to old user bloc

--- lib/presentation/screens/create_project_screen.dart
+++ lib/presentation/screens/create_project_screen.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mimbo/logic/bloc/user_bloc.dart';
 import 'package:mimbo/logic/cubits/project_cubit.dart';
 
 import '../../data/models/projects.dart';
